e96c815437a763f9cd8c9f2c1ec794d2fd884e99
Carolin Rettlinger <rettling@hm.edu>
2020-03-07 10:11:19 +0100

    additional TestSuite

---
+++ src/test/java/HelloWorldAddTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldAddTest {
+
+    @Test
+    public void firstTest(){
+        HelloWorld have = new HelloWorld("Hello");
+        String want = HelloWorld.returnString("HelloWorld");
+        Assert.assertEquals(have.getString(), want);
+    }
+
+    @Test
+    public void secondTest(){
+        HelloWorld have = new HelloWorld("HelloWorld");
+        String want = HelloWorld.returnString("HelloWorld");
+        Assert.assertEquals(have.getString(), want);
+    }
+
+}
